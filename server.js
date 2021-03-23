2f9c3a398dc06b761218190138703d8f4d15a0b1
PORRET Yann <yporret@micropole.com>
2021-03-23 16:14:39 +0100

    CI/CD : lint  + test

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const port = process.env.PORT || 5000;
 
 // This application level middleware prints incoming requests to the servers console, useful to see incoming requests
 app.use((req, res, next) => {
-    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
-    next();
+  console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+  next();
 });
 
 // Configure the bodyParser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 
 // Configure the CORs middleware
@@ -31,18 +31,18 @@ app.use('/api/v1/', api);
 
 // This middleware informs the express application to serve our compiled React files
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'front/build')));
 
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
+  });
 };
 
 // Catch any bad requests
 app.get('*', (req, res) => {
-    res.status(200).json({
-        msg: 'Catch All'
-    });
+  res.status(200).json({
+    msg: 'Catch All',
+  });
 });
 
 // Configure our server to listen on the port defiend by our port variable
