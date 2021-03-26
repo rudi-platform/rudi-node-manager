c97103d787f707061853901d7e40e6f2f014add4
PORRET Yann <yporret@micropole.com>
2021-03-26 16:15:36 +0100

    CICD : Improve Test

---
+++ controllers/controllers.spec.js
@@ -0,0 +1,39 @@
+const {expect} = require('@jest/globals');
+const axios = require('axios');
+const controllers = require('./controllers');
+const {Response} = require('jest-express/lib/response');
+
+
+jest.mock('axios');
+let response;
+
+describe('Controllers', () => {
+  beforeEach(() => {
+    response = new Response();
+  });
+
+  afterEach(() => {
+    response.resetMocked();
+  });
+
+  test('resourcesList should return data', async () => {
+    const data = [{'global_id': 'global',
+      'local_id': 'local',
+      'doi': 'string'}];
+    axios.get.mockImplementation(() => Promise.resolve({data: data}));
+
+    await controllers.resourcesList({}, response, null);
+
+    expect(response.body).toStrictEqual({body: data});
+  });
+  test('getResourceById should return data', async () => {
+    const data = {'global_id': 'global',
+      'local_id': 'local',
+      'doi': 'string'};
+    axios.get.mockImplementation(() => Promise.resolve({data: data}));
+
+    await controllers.getResourceById({params:{id:"aaaa"}}, response, null);
+
+    expect(response.body).toStrictEqual({body: data});
+  });
+});
