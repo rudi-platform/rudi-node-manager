e03c95dde24167c191b61095b11b5801527d31e2
PORRET Yann <yporret@micropole.com>
2021-04-14 17:15:32 +0200

    Deploy : add custom ini

--- config.js
+++ config.js
@@ -1,6 +1,19 @@
 const fs = require('fs');
 const ini = require('ini');
+let configFile;
+let customExist;
+try {
+  customExist = fs.statSync('./rudi_console_proxy_custom.ini').isFile();
+} catch (error) {
+  customExist = false;
+}
+/* TODO IMPROVE (surcharge champs par champs) */
+if (customExist) {
+  configFile = './rudi_console_proxy_custom.ini';
+} else {
+  configFile = './rudi_console_proxy.ini';
+};
+const config = ini.parse(fs.readFileSync(configFile, 'utf-8')); ;
 
-const config = ini.parse(fs.readFileSync('./rudi_console_proxy.ini', 'utf-8'));
 
 module.exports = config;
