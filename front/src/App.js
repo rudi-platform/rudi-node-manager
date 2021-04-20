a86665266c31a88ecf32c36e076e3c2068aaa1f4
PORRET Yann <yporret@micropole.com>
2021-04-20 14:19:28 +0200

    Deploy : test url

--- front/src/App.js
+++ front/src/App.js
@@ -11,6 +11,7 @@ import MetadataDetail from './components/metadataDetail/metadataDetail';
 import Catalogue from './components/catalogue/catalogue';
 import {createBrowserHistory} from 'history';
 
+console.log("process.env.PUBLIC_URL : ", process.env.PUBLIC_URL)
 export const history = createBrowserHistory({
   basename: process.env.PUBLIC_URL,
 });
@@ -33,7 +34,7 @@ class App extends Component {
  */
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <header>
           <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
