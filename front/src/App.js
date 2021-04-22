6f0af302846aa62cc8e8376b7ee3fd16ce9fef30
PORRET Yann <yporret@micropole.com>
2021-04-22 15:58:30 +0200

    Deploy : basename => history

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,7 @@ import {createBrowserHistory} from 'history';
 
 console.log("process.env.PUBLIC_URL : ", process.env.PUBLIC_URL)
 export const history = createBrowserHistory({
-  basename: "/prodmanager-shared",
+  basename: process.env.PUBLIC_URL,
 });
 /*
 TODO :
@@ -33,7 +33,7 @@ class App extends Component {
  */
   render() {
     return (
-      <Router basename= "/prodmanager-shared">
+      <Router history={history}>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <header>
           <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
