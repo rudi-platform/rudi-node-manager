b348bcc73c181f9b03c7fdb116551a82a27b5f28
PORRET Yann <yporret@micropole.com>
2021-04-22 16:38:32 +0200

    Deploy : no history

--- front/src/App.js
+++ front/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
  */
   render() {
     return (
-      <Router history={history} basename="/prodmanager-shared" >
+      <Router basename="/prodmanager-shared" >
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <header>
           <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
