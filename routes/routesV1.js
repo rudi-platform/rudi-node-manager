fbdecde1afa200f79077543bbfc400d7abed0597
PORRET Yann <yporret@micropole.com>
2021-05-07 14:38:07 +0200

    BACK : init user

--- routes/routesV1.js
+++ routes/routesV1.js
@@ -3,7 +3,6 @@ const router = new express.Router();
 const sysController = require('../controllers/sysController');
 const authController = require('./../controllers/authController');
 
-
 router.get('/hash', sysController.getHash);
 router.get('/formUrl', sysController.getFormUrl);
 router.get('/test', sysController.getTest);
@@ -12,5 +11,6 @@ router.post('/register', authController.postRegister);
 router.post('/login', authController.postLogin);
 router.post('/forgot-password', authController.postForgot);
 router.post('/reset-password', authController.postReset);
+
 
 module.exports = router;
