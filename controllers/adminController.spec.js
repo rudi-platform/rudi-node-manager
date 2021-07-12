81eaab1302bd6780461596596e85878c8ec6189f
PORRET Yann <yporret@micropole.com>
2021-07-12 17:29:50 +0200

    Quality : add TU

---
+++ controllers/adminController.spec.js
@@ -0,0 +1,61 @@
+const { expect } = require('@jest/globals');
+const axios = require('axios');
+const controllers = require('./adminController');
+const { Response } = require('jest-express/lib/response');
+
+jest.mock('axios');
+let response;
+
+describe('AdminController', () => {
+  beforeEach(() => {
+    response = new Response();
+  });
+
+  afterEach(() => {
+    response.resetMocked();
+  });
+
+  test('getEnum should return data', async () => {
+    const data = { global_id: 'global', local_id: 'local', doi: 'string' };
+    axios.get.mockImplementation(() => Promise.resolve({ data: data }));
+
+    await controllers.getEnum(null, response, null);
+
+    expect(response.body).toStrictEqual(data);
+  });
+  test('getEnum should return error', async () => {
+    const error = new Error('Error: Request failed with status code 500');
+    let axiosError = {
+      status: 500,
+      response: { error },
+    };
+    axiosError.toJSON = () => axiosError;
+    axios.get.mockImplementation(() => Promise.reject(axiosError));
+
+    await controllers.getEnum(null, response, null);
+
+    expect(response.statusCode).toStrictEqual(501);
+  });
+
+  test('getLicences should return data', async () => {
+    const data = { global_id: 'global', local_id: 'local', doi: 'string' };
+    axios.get.mockImplementation(() => Promise.resolve({ data: data }));
+
+    await controllers.getLicences(null, response, null);
+
+    expect(response.body).toStrictEqual(data);
+  });
+  test('getLicences should return error', async () => {
+    const error = new Error('Error: Request failed with status code 500');
+    let axiosError = {
+      status: 500,
+      response: { error },
+    };
+    axiosError.toJSON = () => axiosError;
+    axios.get.mockImplementation(() => Promise.reject(axiosError));
+
+    await controllers.getLicences(null, response, null);
+
+    expect(response.statusCode).toStrictEqual(501);
+  });
+});
