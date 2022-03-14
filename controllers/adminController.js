c7ae662996b879f00c5bd7a6b9e9bd905bf4dbe7
Olivier Martineau <olivier.martineau@irisa.fr>
2022-03-14 13:17:26 +0000

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -117,7 +117,7 @@ exports.getVersion = (req, res, next) => {
     })
     .then((resRUDI) => {
       const reports = resRUDI.data;
-      res.status(200).json(reports);
+      res.status(200).send(reports);
     })
     .catch((error) => {
       error = errorHandler.error(error, req, { opType: 'get_version' });
