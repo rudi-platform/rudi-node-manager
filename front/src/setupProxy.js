808db2f81b1a1e581f5314f849484a55e0dbffc6
Olivier Martineau <olivier.martineau@irisa.fr>
2022-04-15 18:21:16 +0200

    fix/logout

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -1,12 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const backPath = `${process.env.PUBLIC_URL}/api`;
+const rewriteBackPathKey = `^${backPath}`;
+
 module.exports = function (app) {
   app.use(
-    `${getFrontPath()}/api`,
+    backPath,
     createProxyMiddleware({
       target: 'http://localhost:5000',
       changeOrigin: true,
-      pathRewrite: { [`^/${getFrontPath()}api`]: '/api' },
+      pathRewrite: { [rewriteBackPathKey]: '/api' },
     }),
   );
 };
