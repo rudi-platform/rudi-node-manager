71eb7002b621a674206aeeae8e53a7838e4643ef
Olivier Martineau <olivier.martineau@irisa.fr>
2022-11-24 10:53:41 +0100

    fix: update bcrypt pwd to crypto

--- database/database.js
+++ database/database.js
@@ -277,7 +277,7 @@ exports.dbCreateUser = (openedDb, user) => {
 
 exports.dbHashAndUpdatePassword = async (openedDb, username, password) => {
   const hashedPwd = await hashPassword(password)
-  return this.dbUpdatePassword(openedDb, username, hashedPwd)
+  return await this.dbUpdatePassword(openedDb, username, hashedPwd)
 }
 
 exports.dbUpdatePassword = (openedDb, username, password) => {
