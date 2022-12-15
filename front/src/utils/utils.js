2e44421aff9fb77c9bf51458b9868c5198ef3687
Olivier Martineau <olivier.martineau@irisa.fr>
2022-12-15 10:36:23 +0100

    fix: correct the date displayed

--- front/src/utils/utils.js
+++ front/src/utils/utils.js
@@ -8,7 +8,7 @@ const twoDigits = (n) => `${n}`.padStart(2, '0')
 exports.nowLocaleFormatted = (date) => {
   const d = new Date(date)
   return (
-    `${twoDigits(d.getDay())}/${twoDigits(d.getMonth())}/${d.getFullYear()} ` +
+    `${twoDigits(d.getDate())}/${twoDigits(d.getMonth()+1)}/${d.getFullYear()} ` +
     `${twoDigits(d.getHours())}:${twoDigits(d.getMinutes())}:${twoDigits(d.getSeconds())}`
   )
 }
