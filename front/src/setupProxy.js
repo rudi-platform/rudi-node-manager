48d6094c30cc4fcf54da6fce13f6a41349dccdfb
Olivier Martineau <olivier.martineau@irisa.fr>
2024-05-14 09:35:42 +0200

    wip: back api

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -2,20 +2,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 const backPath = `${process.env.PUBLIC_URL}/api`
 const rewriteBackPathKey = `^${backPath}`
-
-module.exports = function (app) {
+const here = '[createProxyMiddleware]'
+module.exports = (app) => {
   app.use(
     backPath,
     createProxyMiddleware({
       target: 'http://localhost:5005',
       changeOrigin: true,
-      pathRewrite: { [rewriteBackPathKey]: '/api' },
-      // pathRewrite: (path, req) => {
-      //   const pathReplaced = path.replace(new RegExp(rewriteBackPathKey), '/api');
-      //   console.log(req.url, '=>', pathReplaced, '|', req.params, '|', req.query);
-      //   // console.log('path:', path, ' =>', `(${rewriteBackPathKey})`, pathReplaced);
-      //   return pathReplaced;
-      // },
+      // pathRewrite: { [rewriteBackPathKey]: '/api' },
+      pathRewrite: (path, req) => {
+        const pathReplaced = path.replace(new RegExp(rewriteBackPathKey), '/api')
+        console.log(here, req.url, '=>', pathReplaced, ' | ', req.params, ' | ', req.query)
+        // console.log('path:', path, ' =>', `(${rewriteBackPathKey})`, pathReplaced);
+        return pathReplaced
+      },
     })
   )
 }
+console.log('')
