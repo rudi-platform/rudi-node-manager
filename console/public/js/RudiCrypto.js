d3e6975cadbde6867e307ba6422be3e9a5b9b89b
Olivier Martineau <olivier.martineau@irisa.fr>
2024-09-06 08:42:57 +0200

    fix: pub key encryption

--- console/public/js/RudiCrypto.js
+++ console/public/js/RudiCrypto.js
@@ -66,7 +66,7 @@ export function ab2str(arrayBuffer) {
  */
 export async function encryptRsaOaepAesGcm(file, publicKey) {
   // Get file ArrayBuffer and generate an AES-GCM key
-  let [fileArrayBuffer, aezsKey] = await Promise.all([
+  let [fileArrayBuffer, aesKey] = await Promise.all([
     file.arrayBuffer(),
     await crypto.subtle.generateKey({ name: 'AES-GCM', length: 256 }, true, ['encrypt', 'decrypt']),
   ])
