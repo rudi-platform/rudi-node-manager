931e960e20811cb8110e65a39fd82ac30ae90f37
Olivier Martineau <olivier.martineau@irisa.fr>
2024-11-15 17:02:15 +0100

    fix: req in/out/err

--- server.js
+++ server.js
@@ -143,8 +143,8 @@ const launchExpressApp = async ({ catalogUrl, storageUrl }) => {
 
   // This application level middleware prints incoming requests to the servers console, useful to see incoming requests
   managerApp.use((req, reply, next) => {
-    const logReqMsg = `Request <= ${req?.method} ${req?.url} (from ${req?.ip})`
-    log.sysInfo(mod, '', logReqMsg, log.getContext(req, {}))
+    const logReqMsg = `${req?.method} ${req?.url} (from ${req?.ip})`
+    log.sysInfo('req', 'in <', logReqMsg, log.getContext(req, {})) // <= IN
 
     // Redirection for trailing slashes
     // https://stackoverflow.com/a/15773824/1563072
@@ -158,11 +158,11 @@ const launchExpressApp = async ({ catalogUrl, storageUrl }) => {
 
     reply.on('finish', () => {
       if (reply.statusCode < 400) {
-        const okReplyMsg = `=> OK ${reply.statusCode}: ${req.method} ${req.originalUrl}`
-        log.sysInfo(mod, '', okReplyMsg, log.getContext(req, {}))
+        const okReplyMsg = `${reply.statusCode}: ${req.method} ${req.originalUrl}`
+        log.sysInfo('req', 'out >', okReplyMsg, log.getContext(req, {})) // => OK
       } else {
-        const errReplyMsg = `=> ERR ${reply.statusCode} ${reply.statusMessage} > ${req.method} ${req.originalUrl}`
-        log.sysError(mod, '', errReplyMsg, log.getContext(req, {}))
+        const errReplyMsg = `${reply.statusCode} ${reply.statusMessage} > ${req.method} ${req.originalUrl}`
+        log.sysError('req', 'err >', errReplyMsg, log.getContext(req, {})) // => ERR
       }
     })
   })
