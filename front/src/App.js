62e4ed1c2eea4547160d114d15a383b99312bcf3
Olivier Martineau <olivier.martineau@irisa.fr>
2025-01-24 16:42:57 +0100

    fix: eternal front redirect

--- front/src/App.js
+++ front/src/App.js
@@ -202,7 +202,6 @@ export default function App() {
         <div id="root"></div>
 
         <Routes>
-          <Route path="" element={<CatalogueMetadata editMode={isEditor} logout={logout} />} />
           <Route path="/" element={<CatalogueMetadata editMode={isEditor} logout={logout} />} />
           <Route path="metadata" element={<CatalogueMetadata editMode={isEditor} logout={logout} />} />
           <Route path="producer" element={<CatalogueProducer editMode={isEditor} logout={logout} />} />
