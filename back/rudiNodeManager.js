7c47dc42259845f4e2cfdb992d4b0d2e8e9a9e8b
Olivier Martineau <olivier.martineau@irisa.fr>
2025-02-03 15:17:34 +0100

    fix: redirection loop

--- back/rudiNodeManager.js
+++ back/rudiNodeManager.js
@@ -358,7 +358,7 @@ const launchManagerRouter = async ({ catalogUrl, storageUrl }) => {
     })
 
     // Redirecting everything else to the React front
-    managerApp.get('*', (req, reply) => {
+    managerApp.get('/*', (req, reply) => {
       logW(mod, trace, `Redirecting the following URL to the UI: ${req.url} -> ${getRouterFront('/')}`)
       reply.redirect(308, getRouterFront('/'))
     })
